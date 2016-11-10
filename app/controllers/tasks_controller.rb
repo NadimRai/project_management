036a62be403db61d122909f97fb4e298b1218240
NadimRai <rai_nadim@yahoo.com>
2016-11-10 14:21:33 -0800

    Task complete

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,6 @@
 class TasksController < ApplicationController
-	before_action :set_project, only:[:create, :destroy]
+	before_action :set_project
+	before_action :set_task, except:[:create]
 	def create
 	
 	@task = @project.tasks.create(task_params)
@@ -8,13 +9,18 @@ class TasksController < ApplicationController
 end
 
 def destroy
-	@task = @project.tasks.find(params[:id])
+	
 	if @task.destroy
 		flash[:success] = "Task was deleted."
 	else
 		flash[:error] = "Task was not deleted."
 	end
 	redirect_to @project
+end
+
+def complete
+	@task.update_attribute(:completed_at, Time.now)
+	redirect_to @project, notice: "Task Completed"
 end
 private
 
@@ -24,6 +30,10 @@ end
 
 def task_params
 	params.require(:task).permit(:content)
+end
+
+def set_task
+	@task = @project.tasks.find(params[:id])
 end
 
 end
